f2a8e675fe9807e5d731a63b11dcf6c3ee1cf31f
David Gómez Bernal <dagobe2811@fpuna.edu.py>
2023-11-27 18:20:36 -0300

    Se inserta codigo para descargar datos de articulos en pdf

--- src/app/components/list-products/list-products.component.ts
+++ src/app/components/list-products/list-products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-list-products',
@@ -28,4 +30,34 @@ export class ListProductsComponent implements OnInit{
       console.log('Product deleted');
     })
   }
+
+  public downloadPDF() {
+    // Obtener el elemento 'productos'
+    const DATA = document.getElementById('productos');
+
+    if (DATA) { // Verificar si el elemento existe
+      const doc = new jsPDF('p', 'pt', 'a4');
+      const options = {
+        background: 'white',
+        scale: 3
+      };
+
+      html2canvas(DATA, options).then((canvas) => {
+        const img = canvas.toDataURL('image/PNG');
+
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        doc.save(`${new Date().toISOString()}_productos.pdf`);
+      });
+    } else {
+      console.error("Elemento 'productos' no encontrado.");
+    }
+  }
+
 }
+
+
